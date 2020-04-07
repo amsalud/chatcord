733037a535b89124113b0f701a7f04fe7fb5cf5e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-06 20:45:34 -0400

    Emit message event

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run when client connects
 io.on('connection', socket => {
     console.log('New WS Connection...');
+    socket.emit('message', 'Welcome to ChatCord!')
 });
 
 const PORT = 3000 || process.env.PORT;
