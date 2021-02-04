6071e763b76cf507cf6e0a0a7db55673d51c100d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-04 12:35:21 -0500

    Add routes

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,38 +1,17 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import history from './history';
+import JoinChannel from './components/JoinChannel';
+import Chatroom from './components/Chatroom';
 
 const App = () => {
   return (
-    <div className="join-container">
-      <header className="join-header">
-        <h1><i className="fas fa-smile"></i> ChatCord</h1>
-      </header>
-      <main className="join-main">
-        <form action="chat.html">
-          <div className="form-control">
-            <label for="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="Enter username..."
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label for="room">Room</label>
-            <select name="room" id="room">
-              <option value="JavaScript">JavaScript</option>
-              <option value="Python">Python</option>
-              <option value="PHP">PHP</option>
-              <option value="C#">C#</option>
-              <option value="Ruby">Ruby</option>
-              <option value="Java">Java</option>
-            </select>
-          </div>
-          <button type="submit" className="btn">Join Chat</button>
-        </form>
-      </main>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={JoinChannel} />
+        <Route path="/chatroom" exact component={Chatroom} />
+      </Switch>
+    </Router>
   );
 };
 export default App;
