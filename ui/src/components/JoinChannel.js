1bb6184e5616752d9c928d250bf295e4556f79fa
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-04 12:47:58 -0500

    Update JoinChannel.js

--- ui/src/components/JoinChannel.js
+++ ui/src/components/JoinChannel.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const JoinChannel = () => {
+
+    const [username, setUsername] = useState('');
+    const [channels, setChannels] = useState(['Javascript', 'Python', 'PHP', 'C#', 'Ruby', 'Java']);
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(username);
+    }
 
     return (
         <div className="join-container">
@@ -8,26 +16,17 @@ const JoinChannel = () => {
                 <h1><i className="fas fa-smile"></i> ChatCord</h1>
             </header>
             <main className="join-main">
-                <form action="chat.html">
+                <form onSubmit={onSubmit}>
                     <div className="form-control">
                         <label htmlFor="username">Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            id="username"
-                            placeholder="Enter username..."
-                            required
-                        />
+                        <input type="text" name="username" id="username" placeholder="Enter username..." required value={username} onChange={e => setUsername(e.target.value)} />
                     </div>
                     <div className="form-control">
                         <label htmlFor="room">Room</label>
                         <select name="room" id="room">
-                            <option value="JavaScript">JavaScript</option>
-                            <option value="Python">Python</option>
-                            <option value="PHP">PHP</option>
-                            <option value="C#">C#</option>
-                            <option value="Ruby">Ruby</option>
-                            <option value="Java">Java</option>
+                            {
+                                channels.map((channel, index) => (<option key={index}>{channel}</option>))
+                            }
                         </select>
                     </div>
                     <button type="submit" className="btn">Join Chat</button>
