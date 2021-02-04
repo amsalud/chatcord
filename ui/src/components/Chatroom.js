7173ca0fab81830ff7dd3568fc0c8189dac19963
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-04 12:58:22 -0500

    Fix warnings

--- ui/src/components/Chatroom.js
+++ ui/src/components/Chatroom.js
@@ -2,30 +2,30 @@ import React from 'react';
 
 const Chatroom = () => {
     return (
-        <div class="chat-container">
-            <header class="chat-header">
-                <h1><i class="fas fa-smile"></i> ChatCord</h1>
-                <a href="index.html" class="btn">Leave Room</a>
+        <div className="chat-container">
+            <header className="chat-header">
+                <h1><i className="fas fa-smile"></i> ChatCord</h1>
+                <a href="index.html" className="btn">Leave Room</a>
             </header>
-            <main class="chat-main">
-                <div class="chat-sidebar">
-                    <h3><i class="fas fa-comments"></i> Room Name:</h3>
-                    {/* <h2 id="room-name"></h2> */}
-                    <h3><i class="fas fa-users"></i> Users</h3>
+            <main className="chat-main">
+                <div className="chat-sidebar">
+                    <h3><i className="fas fa-comments"></i> Room Name:</h3>
+                    <h2 id="room-name">RoomName</h2>
+                    <h3><i className="fas fa-users"></i> Users</h3>
                     <ul id="users"></ul>
                 </div>
-                <div class="chat-messages"></div>
+                <div className="chat-messages"></div>
             </main>
-            <div class="chat-form-container">
+            <div className="chat-form-container">
                 <form id="chat-form">
                     <input
                         id="msg"
                         type="text"
                         placeholder="Enter Message"
                         required
-                        autocomplete="off"
+                        autoComplete="off"
                     />
-                    <button class="btn"><i class="fas fa-paper-plane"></i> Send</button>
+                    <button className="btn"><i className="fas fa-paper-plane"></i> Send</button>
                 </form>
             </div>
         </div>
