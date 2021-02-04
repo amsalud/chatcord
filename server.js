942472f7ae794005a006ec5f1b6ff7237dc39b1a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-04 12:18:31 -0500

    Change Port

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
