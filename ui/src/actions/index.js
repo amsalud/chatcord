81604bb807bf11553370b7fbdd735189426037d6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-08 18:34:32 -0500

    Redirect back home

--- ui/src/actions/index.js
+++ ui/src/actions/index.js
@@ -1,8 +1,14 @@
-import { SET_USER, SET_CHANNEL } from './types'
+import { SET_USER, SET_CHANNEL, CLEAR_CHANNEL, CLEAR_USER } from './types'
 import history from '../history';
 
 export const joinChannel = (username, channel) => dispatch => {
     dispatch({ type: SET_USER, payload: username });
     dispatch({ type: SET_CHANNEL, payload: channel });
     history.push('/chatroom');
+};
+
+export const leaveRoom = () => dispatch => {
+    dispatch({ type: CLEAR_USER });
+    dispatch({ type: CLEAR_CHANNEL });
+    history.push('/');
 };
