78909136d3e59d3c916f60cf2594ae3122e6c3dc
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-09 22:21:04 -0500

    Add SET_CHATROOM_USERS action type

--- ui/src/actions/types.js
+++ ui/src/actions/types.js
@@ -1,5 +1,6 @@
 export const SET_USER = 'SET_USER';
 export const SET_CHANNEL = 'SET_CHANNEL';
 export const SET_MESSAGE = 'SET_MESSAGE';
+export const SET_CHATROOM_USERS = 'SET_CHATROOM_USERS';
 export const CLEAR_USER = 'CLEAR_USER';
 export const CLEAR_CHANNEL = 'CLEAR_CHANNEL';
