fdf8f59d194d1e39f228323a7bf06d80c21257c5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-10 11:30:33 -0500

    Update socketEventTypes.js

--- ui/src/utils/socketEventTypes.js
+++ ui/src/utils/socketEventTypes.js
@@ -1,4 +1,4 @@
-export const JOIN_ROOM = 'joinRoom';
+export const JOIN_CHANNEL = 'joinRoom';
 export const CHATROOM_USERS_UPDATE = 'roomUsers';
 export const NEW_MESSAGE = 'message';
 export const SEND_MESSAGE = 'chatMessage';
