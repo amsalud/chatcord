be38cfb1809c074dd141c7ce68d99c8c83bd7c6c
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-10 11:49:29 -0500

    Fix export

--- chat/utils/socketEventTypes.js
+++ chat/utils/socketEventTypes.js
@@ -1,5 +1,7 @@
-export const JOIN_CHANNEL = 'joinRoom';
-export const CHATROOM_USERS_UPDATE = 'roomUsers';
-export const NEW_MESSAGE = 'message';
-export const SEND_MESSAGE = 'chatMessage';
+module.exports = {
+    JOIN_CHANNEL: 'JOIN_CHANNEL',
+    CHATROOM_USERS_UPDATED: 'CHATROOM_USERS_UPDATED',
+    NEW_MESSAGE: 'NEW_MESSAGE',
+    SEND_MESSAGE: 'SEND_MESSAGE'
+};
 
