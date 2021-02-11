4db65d719255a0c88df38035ee9046cef7493c10
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-11 11:22:52 -0500

    Update users.js

--- chat-api/utils/users.js
+++ chat-api/utils/users.js
@@ -1,8 +1,8 @@
 const users = [];
 
 // Join user to chat
-function userJoin(id, username, room){
-    const user = {id, username, room};
+function userJoin(id, username, channel){
+    const user = {id, username, channel};
     users.push(user);
     return user;
 }
@@ -21,13 +21,13 @@ function userLeave(id){
 }
 
 // Get room users
-function getRoomUsers(room){
-    return users.filter((user => user.room === room));
+function getChannelUsers(channel){
+    return users.filter((user => user.channel === channel));
 }
 
 module.exports = {
     userJoin,
     getCurrentUser,
     userLeave,
-    getRoomUsers
+    getChannelUsers
 }
